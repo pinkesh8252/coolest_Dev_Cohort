17d812349249363718049e8d5ffbd1cdd09d3fc3
pinkesh8252 <pinkeshk213@gmail.com>
2024-12-16 08:02:07 +0530

    vlc 2nd class

---
+++ VLC/fair/script.js
@@ -0,0 +1,5 @@
+
+const SpeedUp = document.querySelector("#SpeedUp");
+const SpeedDown = document.querySelector("#SpeedDown");
+const VolumeUp = document.querySelector("#VolumeUp");
+const VolumeDown = document.querySelector("#VolumeDown");
